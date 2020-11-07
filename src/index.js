7aa42bdaf116fd2de979fd144df315b27476e3a1
flavioro <flavioro@gmail.com>
2020-11-06 22:36:34 -0300

    Add component Children

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import First from './components/First'
-import WithParameters from './components/WithParameters'
+// import First from './components/First'
+// import WithParameters from './components/WithParameters'
+import WithChildren from './components/WithChildren'
 
 import './index.css'
 
 ReactDOM.render(
   <div>
-    <First/>
+    <WithChildren>
+      <ul>
+        <li>Ana</li>
+        <li>Bia</li>
+        <li>Carlos</li>
+        <li>Daniel</li>
+      </ul>
+    </WithChildren>
+    {/* <First/>
     <WithParameters title='This is title'
-      subtitle='This is subtitle'/>
+      subtitle='This is subtitle'/> */}
   </div>,
   document.getElementById('root')
 );
